cf1b6b7762cc16b0165e7f7a4f3e579810dbdcd2
Kunal Singh <kunalindia59@gmail.com>
2026-08-03 01:40:13 +0530

    Idempotency key fix

--- primary-backend/src/routes/order.route.ts
+++ primary-backend/src/routes/order.route.ts
@@ -1,9 +1,10 @@
 import express from "express"
 
 import { newOrder } from "../controllers/orders.controller.js"
+import { authMiddleWare } from "../middlewares/authMiddleware.js"
 
 const orderRouter = express.Router()
 
-orderRouter.post('/newOrder', newOrder)
+orderRouter.post('/newOrder', authMiddleWare, newOrder)
 
 export default orderRouter
