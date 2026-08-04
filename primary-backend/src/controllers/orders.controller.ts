bc3b4816f028bdf360d2da9c37df6a972cb8feac
Kunal Singh <kunalindia59@gmail.com>
2026-08-04 12:39:10 +0530

    Route handlers for cancelling an order and getting all orders placed

--- primary-backend/src/controllers/orders.controller.ts
+++ primary-backend/src/controllers/orders.controller.ts
@@ -3,6 +3,7 @@ import { createOrder, type OrderRequest } from "../helpers/order.js"
 import { redisClient } from "../index.js"
 import type { Decimal } from "@prisma/client/runtime/client"
 import { Prisma } from "@prisma/client"
+import { prisma } from "../db.js"
 
 export const newOrder = async (req: any, res: express.Response) => {
 
@@ -66,4 +67,78 @@ export const newOrder = async (req: any, res: express.Response) => {
         })
     }
 
+}
+
+export const getOrders = async (req: any, res: express.Response) => {
+    try {
+        const user_id = req.id
+        const orders = await prisma.orders.findMany({
+            where: {
+                user_id: user_id
+            },
+            select: {
+                id: true,
+                side: true,
+                status: true,
+                price: true,
+                quantity: true,
+                remaining_quantity: true,
+                created_at: true,
+                updated_at: true
+            }
+        })
+        if (!orders) {
+            res.status(404).json({
+                message: 'Orders not found, please place an order',
+                valid: false
+            })
+            return
+        }
+        res.status(200).json({
+            orders,
+            valid: true
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            message: "Something went wrong",
+            valid: false
+        })
+    }
+}
+
+export const cancelOrder = async (req: express.Request, res: express.Response) => {
+    try {
+        const order_id = req.body.order_id;
+        const cancelled = await prisma.orders.update({
+            where: {
+                id: order_id
+            },
+            data: {
+                status: "CANCELLED"
+            }
+        })
+        const book = await redisClient.get('orderBook')
+        let orderBook = JSON.parse(book!)
+        orderBook = orderBook.filter((order: any) => order.id !== order_id)
+        await redisClient.set('orderBook', JSON.stringify(orderBook))
+        if (!cancelled) {
+            await redisClient.lPush('ORDER_UPDATE_RETRY', JSON.stringify({ id: order_id, data: { status: "CANCELLED" } }))
+            res.status(403).json({
+                message: "Unable to cancel order at the moment",
+                valid: false
+            })
+            return
+        }
+        res.status(200).json({
+            valid: true,
+            message: 'Order cancelled'
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            message: "Something went wrong",
+            valid: false
+        })
+    }
 }
