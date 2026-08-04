64ca96f8d58f767d27ccbbc81aee1026224f4a76
Kunal Singh <kunalindia59@gmail.com>
2026-08-04 12:39:44 +0530

    New routes in orders /cancel and /getOrders

--- primary-backend/src/routes/order.route.ts
+++ primary-backend/src/routes/order.route.ts
@@ -1,10 +1,12 @@
 import express from "express"
 
-import { newOrder } from "../controllers/orders.controller.js"
+import { cancelOrder, getOrders, newOrder } from "../controllers/orders.controller.js"
 import { authMiddleWare } from "../middlewares/authMiddleware.js"
 
 const orderRouter = express.Router()
 
 orderRouter.post('/newOrder', authMiddleWare, newOrder)
+orderRouter.get('/getOrders', authMiddleWare, getOrders)
+orderRouter.post('/cancel', authMiddleWare, cancelOrder)
 
 export default orderRouter
